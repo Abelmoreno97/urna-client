e939cd52f9c7334f8363fba5dd55ae5bed3b1251
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-25 16:33:35 -0300

    feat: add user register

--- src/views/profilecomplete/profilecompelte.jsx
+++ src/views/profilecomplete/profilecompelte.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react';
-import { Input, Button, Stack, Radio, RadioGroup, FormControl, FormLabel } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import {
+  Input,
+  Button,
+  Stack,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  Select,
+} from "@chakra-ui/react";
+import { cookie } from "../../utils";
 
 const ProfileComplete = () => {
-  const [value, setValue] = useState('1');
   const [profileInfo, setProfileInfo] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    username: "",
+    email: "",
+    avatar: "",
+    region_id: "",
   });
+  const [regions, setRegions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/regions")
+      .then((res) => res.json())
+      .then((res) => setRegions(res.data))
+      .catch((err) => console.log(err));
+
+    const emailFromCookies = cookie.get("email");
+    const usernameFromCookies = cookie.get("username");
+    setProfileInfo((prev) => {
+      return { ...prev, email: emailFromCookies, username: usernameFromCookies };
+    });
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,40 +40,35 @@ const ProfileComplete = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
     // Do something with the form data, for example, send it to a server.
-    fetch('/api/save-profile', {
-      method: 'POST',
+    fetch("http://localhost:3001/users", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(profileInfo),
+      body: JSON.stringify({ profileInfo }),
     })
       .then((response) => response.json())
       .then((data) => {
         // Process the response from the server if needed
-        console.log('Server response:', data);
+        console.log("Server response:", data);
       })
       .catch((error) => {
         // Handle any errors that occurred during the request
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
   };
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-    
-  //   console.log(profileInfo);
-  // };
 
   return (
     <div>
       <h1>UserComplete</h1>
       <form onSubmit={handleSubmit}>
-        <RadioGroup onChange={setValue} value={value}>
+        <RadioGroup onChange={(e) => handleChange({ target: { name: "avatar", value: e } })}>
           <Stack direction="row">
-            <Radio value="1">First</Radio>
-            <Radio value="2">Second</Radio>
-            <Radio value="3">Third</Radio>
+            <Radio value="349e8669-c6da-4b28-9df3-ea51afaf05f1">First</Radio>
+            <Radio value="349e8669-c6da-4b28-9df3-ea51afaf05f2">Second</Radio>
+            <Radio value="349e8669-c6da-4b28-9df3-ea51afaf05f3">Third</Radio>
           </Stack>
         </RadioGroup>
         <FormControl isRequired>
@@ -59,21 +77,15 @@ const ProfileComplete = () => {
         </FormControl>
         <FormControl isRequired>
           <FormLabel>Email</FormLabel>
-          <Input type="email" name="email" value={profileInfo.email} onChange={handleChange} />
+          <Input type="email" name="email" value={profileInfo.email} disabled />
         </FormControl>
-        <FormControl isRequired>
-          <FormLabel>Password</FormLabel>
-          <Input type="password" name="password" value={profileInfo.password} onChange={handleChange} />
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel>Repeat Password</FormLabel>
-          <Input
-            type="password"
-            name="confirmPassword"
-            value={profileInfo.confirmPassword}
-            onChange={handleChange}
-          />
-        </FormControl>
+        <Select placeholder="Select option" name="region_id" onChange={handleChange}>
+          {regions?.map((region, index) => (
+            <option key={index} value={region._id}>
+              {region.name}
+            </option>
+          ))}
+        </Select>
         <Button mt={4} colorScheme="teal" type="submit">
           Submit
         </Button>
@@ -83,4 +95,3 @@ const ProfileComplete = () => {
 };
 
 export default ProfileComplete;
-

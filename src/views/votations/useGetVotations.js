c7636c095c4fec366b4e34abb7f4e81577ada87a
FrancoFrias23 <lockke2323@gmail.com>
2023-07-26 15:09:45 -0300

    feat: add useGetVotation

---
+++ src/views/votations/useGetVotations.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+import Votation from '../../services/Votation';
+import { useDispatch, useSelector } from "react-redux";
+import { setData, setError, setStatus } from "../../redux/features/votationsSlice";
+
+
+export function useGetVotations() {
+  const { data, status, error } = useSelector((state) => state.votations);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setStatus("loading"));
+      Votation.getAll()
+      .then((res) => {
+        dispatch(setData(res.data))})
+      .catch((err) => dispatch(setError(err)));
+  }, []);
+
+  return { data, status, error };
+}

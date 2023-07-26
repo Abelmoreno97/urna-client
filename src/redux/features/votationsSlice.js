c0011ae5b6cf34bb5d94c8d1cd33e7f054018636
FrancoFrias23 <lockke2323@gmail.com>
2023-07-26 15:08:54 -0300

    feat: add votationSlice

---
+++ src/redux/features/votationsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import Votation from '../../services/Votation';
+
+const initialState = {
+  data: [],
+  status: "loading",
+  error: null,
+}
+
+export const votationsSlice = createSlice({
+  name: 'votations',
+  initialState,
+  reducers: {
+    setData:(state,action) => {
+          state.data= action.payload
+          state.status="succeded"
+      },
+    setStatus:(state,action) => {
+         state.status=action.payload
+        
+      },
+    setError:(state,action) => {
+         state.error=action.payload
+        
+      },
+    
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setData, setStatus,setError } = votationsSlice.actions
+
+export default votationsSlice.reducer

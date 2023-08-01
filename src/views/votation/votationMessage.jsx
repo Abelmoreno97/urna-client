f276d1abcd2cc8d78caef5ec92c24cefdd18ab4f
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-08-01 14:29:36 -0300

    feat: dynamic data renderer

--- src/views/votation/votationMessage.jsx
+++ src/views/votation/votationMessage.jsx
@@ -6,47 +6,33 @@ import Navbar from "../../components/navbar/navbar";
 import { Image, VStack } from "@chakra-ui/react";
 import chatleft from "../../assets/chat-left.svg";
 import heart from "../../assets/heart.svg";
+import useGetVotationDetails from "../votationform/useGetVotationDetails";
 
 const VotationMessage = () => {
   const { id } = useParams();
-  console.log(id);
 
-  // pedir detalles de la votacion
+  const { votation, votes, sortedOptions } = useGetVotationDetails(id);
+  console.log(votation);
+  console.log(votes);
 
   return (
     <div>
       <div className={Gstyle.cont}>
-        <h1>Votation</h1>
-        <Votebar />
+        <h1>{votation?.title}</h1>
+        <Votebar sortedOptions={sortedOptions} />
         <div>
           <div className={style.votecont}>
-            <div className={style.votecard}>
-              Voto acompañado de mensaje{" "}
-              <VStack>
-                <Link to="../votations/votation/messages/msgdetail">
-                  <Image src={chatleft}></Image>
-                </Link>
-                <Image src={heart}></Image>
-              </VStack>
-            </div>
-            <div className={style.votecard}>
-              Voto acompañado de mensaje{" "}
-              <VStack>
-                <Link to="../votations/votation/messages/msgdetail">
-                  <Image src={chatleft}></Image>
-                </Link>
-                <Image src={heart}></Image>
-              </VStack>
-            </div>
-            <div className={style.votecard}>
-              Voto acompañado de mensaje{" "}
-              <VStack>
-                <Link to="../votations/votation/messages/msgdetail">
-                  <Image src={chatleft}></Image>
-                </Link>
-                <Image src={heart}></Image>
-              </VStack>
-            </div>
+            {votes?.map((vote, i) => (
+              <div key={"voteMsg" + i} className={style.votecard}>
+                {vote?.comment}
+                <VStack>
+                  <Link to="../votations/votation/messages/msgdetail">
+                    <Image src={chatleft}></Image>
+                  </Link>
+                  <Image src={heart}></Image>
+                </VStack>
+              </div>
+            ))}
           </div>
         </div>
       </div>

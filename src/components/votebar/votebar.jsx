5dd73178eda76c3c686371471126ae54dd4842d4
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-08-01 12:42:26 -0300

    feat: dynamic data rendering

--- src/components/votebar/votebar.jsx
+++ src/components/votebar/votebar.jsx
@@ -1,24 +1,19 @@
 import { HStack, Image, VStack } from "@chakra-ui/react";
 import person from "../../assets/person.svg";
 
-const Votebar = () => {
-    
-    return (
-        <HStack>
-          <VStack>
-            <Image w={"50px"} h={"50px"}  src={person} alt='opcion'></Image>
-            <p>opcion 1</p>
-          </VStack>
-          <VStack>
-            <Image w={"50px"} h={"50px"} src={person} alt='opcion'></Image>
-            <p>opcion 2</p>
-          </VStack>
-          <VStack>
-            <Image w={"50px"} h={"50px"}  src={person} alt='opcion'></Image>
-            <p>opcion 3</p>
-          </VStack>
-        </HStack>
-    );
-}
+const Votebar = ({ sortedOptions }) => {
+  return (
+    <HStack>
+      {sortedOptions.slice(0, 3).map((option, i) => (
+        <VStack key={"sortedOption" + i}>
+          <Image w={"75px"} h={"75px"} src={option.images[0] || person} alt="opcion"></Image>
+          <p>{option.title}</p>
+          <span>{option.stats.ratio || "ratio"}%</span>
+          <span>{option.stats.votes || "cantidad"} votos</span>
+        </VStack>
+      ))}
+    </HStack>
+  );
+};
 
 export default Votebar;

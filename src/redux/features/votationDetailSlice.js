95cb4a6e95f7b3817b4a5bddaea7017c418b773f
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-08-03 16:56:57 -0300

    feat: add updateLikes

--- src/redux/features/votationDetailSlice.js
+++ src/redux/features/votationDetailSlice.js
@@ -20,10 +20,29 @@ export const votationDetailSlice = createSlice({
     setError: (state, action) => {
       state.error = action.payload;
     },
+    voteAddLike(state, action) {
+      const { vote_id, user_id } = action.payload;
+      state.data.votes = state.data.votes.map((vote) => {
+        if (vote._id === vote_id) {
+          vote.likes = [...vote.likes, user_id];
+        }
+        return vote;
+      });
+    },
+    voteRemoveLike(state, action) {
+      const { vote_id, user_id } = action.payload;
+      state.data.votes = state.data.votes.map((vote) => {
+        if (vote._id === vote_id) {
+          vote.likes = vote.likes.filter((like) => like !== user_id);
+        }
+        return vote;
+      });
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setData, setStatus, setError } = votationDetailSlice.actions;
+export const { setData, setStatus, setError, voteAddLike, voteRemoveLike } =
+  votationDetailSlice.actions;
 
 export default votationDetailSlice.reducer;

00e3832785c39af7fd71263c044e9be3271c6ca4
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-08-04 10:07:57 -0300

    feat: add create method in VoteRepository

--- src/repositories/Vote.js
+++ src/repositories/Vote.js
@@ -1,4 +1,14 @@
 const baseURL = "http://localhost:3001";
 export default {
-  getResponses: (id) => fetch(baseURL + "/responses/vote/" + id, {method:"GET",credentials:"include"}).then((res) => res.json()),
+  getResponses: (id) =>
+    fetch(baseURL + "/responses/vote/" + id, { method: "GET", credentials: "include" }).then(
+      (res) => res.json()
+    ),
+  create: (voteData) =>
+    fetch(baseURL + "/votes", {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({ voteData }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => res.json()),
 };

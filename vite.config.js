973e26e31d3787c7d0717fd407e9aa1e5606a6af
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-08-14 11:44:55 -0300

    build: add absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
       usePolling: true,
     },
   },
-})
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "src/"),
+    },
+  },
+});
